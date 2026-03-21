89c5aecc445dc7b91c54fccaab086a13fb1ba0e8
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-21 16:57:01 +0900

    :memo: tag定義方法変更

--- envs/compute_linux.tf
+++ envs/compute_linux.tf
@@ -128,9 +128,9 @@ resource "oci_core_instance" "oracle_instance" {
     user_data           = base64encode(file("./userdata/oraclelinux_init.sh"))
   }
   defined_tags = {
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
-    "Compute.CloudAgent"                                                                                                         = "oracle"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag.key_env.name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag.key_managedbyterraform.name) = "true"
+    "Compute.CloudAgent"                                                                                  = "oracle"
   }
   lifecycle {
     ignore_changes = [metadata]
